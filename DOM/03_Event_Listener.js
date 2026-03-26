701a080508de90ff74e2befd9b0cae873ac98f42
sarthak <231179030+MustbeSarthak@users.noreply.github.com>
2026-03-26 11:43:16 +0530

    Still on events

--- DOM/03_Event_Listener.js
+++ DOM/03_Event_Listener.js
@@ -1,6 +1,8 @@
 // Select the element/id/attribute first
 // Then add event listner
 
+// const { json } = require("body-parser");
+
 
 let button = document.querySelector("button");
 button.addEventListener("click", function(){
@@ -14,3 +16,21 @@ button.addEventListener("click", function(){
 
 
 // button.removeEventListener , this will remove the event listner 
+
+// There are various types of event listner 
+let input = document.querySelector("input");
+// This event listner will show what you have written word-by-word 
+input.addEventListener("input", function(data){
+    // WHy this? SO whenever we backspace it does'nt shows the null
+    if(data.data !== null){
+        console.log(data.data);
+    }
+});
+
+
+// Selection Listners
+// To track the changes or track which option is chosen by the user we use " change" Listner
+let Select = document.getElementById("sel");
+Select.addEventListener("change", function(sel){
+    console.log("Fruit Selected: ", sel.target.value);  
+});
